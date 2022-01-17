21b87e3540a022c9877d95dcf4ccc6a45b44113a
Nikolaos <passalis@csd.auth.gr>
2022-01-17 09:29:40 +0200

    Device autodetect

--- p_est.py
+++ p_est.py
@@ -9,38 +9,32 @@ import os
 
 class PowerEstimator:
 
-    def __init__(self, device='nx', idle_load_duration=3, idle_load_samples=10, sampling_rate=30):
+    def __init__(self, idle_load_duration=3, idle_load_samples=10, sampling_rate=30):
+        """
+        Creates a PowerEstimator object for measuring energy consumption
+        :param idle_load_duration: Total time duration for estimating idle energy consumption
+        :param idle_load_samples: Number of samples to be used for idle energy consumption estimation
+        :param sampling_rate: Sampling rate (Hz) to be used when estimating the energy consumption
         """
 
-        :param device: NVIDIA device that is used (either 'nx' or 'tx2' )
-        :param idle_load_duration: Total time duration for estimating idle power consumption
-        :param idle_load_samples: Number of samples to be used for idle power consumption estimation
-        :param sampling_rate: Sampling rate (Hz) to be used when estimating the power consumption
-        """
+        self.devices = []
+        self._device_scan('/sys/bus/i2c/drivers/ina3221x/')
+        self._device_scan('/sys/devices/3160000.i2c/i2c-0/')
+        print("[OpenDR PowerLogger] Found the %d power devices" % (len(self.devices)))
 
-        if device == 'nx' or device == 'agx':
-            base_path = '/sys/bus/i2c/drivers/ina3221x/'
-        elif device == 'tx2':
-            base_path = '/sys/devices/3160000.i2c/i2c-0/'
-        else:
-            print("[OpenDR PowerLogger] Device not supported")
-            assert False
+        self.idle_load = 0
+        self._estimate_standby_load(idle_load_duration, idle_load_samples)
+        self.sampling_interval = 1.0 / sampling_rate
 
+    def _device_scan(self, base_path):
         # Scan for a maximum of 5 sensors
         files = ['iio_device/in_power' + str(i) + '_input' for i in range(5)]
-        self.devices = []
         for _, folders, _ in os.walk(base_path):
             for folder in folders:
                 for file in files:
                     cur_path = os.path.join(base_path, folder, file)
                     if os.path.exists(cur_path):
                         self.devices.append(cur_path)
-        print("[OpenDR PowerLogger] Found the %d power devices" % (len(self.devices)))
-        print(self.devices)
-
-        self.idle_load = 0
-        self._estimate_standby_load(idle_load_duration, idle_load_samples)
-        self.sampling_interval = 1.0 / sampling_rate
 
     def _get_instant_power(self):
         total_power = 0
