16f4ed683c4903f14998641d231abac080cb24d0
Nikolaos Passalis <passalis@users.noreply.github.com>
2022-01-17 09:51:27 +0200

    Update p_est.py

--- p_est.py
+++ p_est.py
@@ -20,7 +20,7 @@ class PowerEstimator:
         self.devices = []
         self._device_scan('/sys/bus/i2c/drivers/ina3221x/')
         self._device_scan('/sys/devices/3160000.i2c/i2c-0/')
-        print("[OpenDR PowerLogger] Found the %d power devices" % (len(self.devices)))
+        print("[OpenDR PowerLogger] Found %d power devices" % (len(self.devices)))
 
         self.idle_load = 0
         self._estimate_standby_load(idle_load_duration, idle_load_samples)
